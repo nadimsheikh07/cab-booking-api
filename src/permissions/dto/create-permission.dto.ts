0e83e791ed15774a1c8ad651e5e95dfa2c284310
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-06-19 17:00:08 +0530

    u[date

--- src/permissions/dto/create-permission.dto.ts
+++ src/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePermissionDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreatePermissionDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  module?: string;
+}
--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,1 +1,28 @@
-export class Permission {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('permissions')
+export class Permission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  module: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
